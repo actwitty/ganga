7d5240005005a0ffe0020dad8c08c2367bf3bf09
Sudhanshu Saxena <sudhanshu@actwitty.com>
2012-10-02 13:41:11 +0530

    Fixed configuration of Mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,15 @@ Ganga::Application.configure do
   config.action_mailer.default :charset => "utf-8"
 
 
+  ActionMailer::Base.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
 
 end
