a37def770b4b743bd2fec357b3fbb737d8978409
amartyadas <amartya@actwitty.com>
2012-11-21 12:58:15 +0530

    Fix for Font Size for Button and Dynamic Variable Problem

--- public/rbt_merge.js
+++ public/rbt_merge.js
@@ -4107,6 +4107,9 @@ testGanga();
 
 
 
+
+
+
 /****************************[[include.js]]*************************************/ 
 
 
@@ -4264,7 +4267,7 @@ trigger_fish.rbT.templateLib = {
               },
              '11' : {
                   key :'rb.t.nr.button_fontSize',
-                  value :'14'
+                  value :'16'
               },
              '12' : {
                   key :'rb.t.cr.button_background_on_focus',
@@ -4743,7 +4746,7 @@ trigger_fish.rbT.rbTemplTopbarGenericTwitterfollowHTML=' <div id="rbTopbarGeneri
 /****************************[[./templates/topbars/rbTemplTopbarGenericNormal.js]]*************************************/ 
 
 
-trigger_fish.rbT.rbTemplTopbarGenericNormalHTML='<div id="rbTopbarGenericNormalBaseContainer">  <!-- -->  <link rel="stylesheet" type="text/css" href="https://s3.amazonaws.com/actwitty_ganga/css/common_styles.css">  <style>        .rbTpGenDyn{      position: fixed;      top: 0px;      left: 0px;         z-index: {{1}};          background: {{2}} !important;      border-top: 1px solid #CC412A !important;      border-bottom: 1px solid #CC412A !important;    }    .rbTpGenDyn .barDyn{      width: {{3}}px;      height: {{4}}px;      vertical-align: middle;      position: relative;    }    .rbTpGenDyn .rbDynText     {       color:{{5}};       font-size: {{6}}px;       font-family: {{7}};       font-weight:{{8}};       white-space: nowrap;       overflow:hidden;     }          .rbTpGenDyn .rbDynLeftText{      position: absolute;      left:20px;       width:40%;      top: 10px;      text-align: center;      margin-right: 20px;     }          .rbTpGenDyn .rbDynRightText{      position: absolute;      right: 20px;       top: 10px;          width:40%;       text-align: center;         margin-left: 20px;     }     .rbTpGenDyn .rbBtnDyn {          padding: 5px;          text-shadow: none !important;        border: 1px solid #BBB;        border: 1px solid #BBB;        border-color: #E6E6E6 #E6E6E6 #BFBFBF;        border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);        -webkit-border-radius: 4px;        -moz-border-radius: 4px;        border-radius: 4px;        -webkit-transition: background 1s ease;        -moz-transition: background 1s ease;        -o-transition: background 1s ease;        transition: background 1s ease;                    position: absolute;              width:100px;        left: 42%;        top:7%;        height:20px;        margin-right: 20px;                            background-color: {{9}} !important;        color: {{10}} !important; !important;        font-size: {{11}}px; !important;         text-align: center;          white-space: nowrap;          overflow: hidden;         text-overflow: ellipsis;     }     .rbTpGenDyn .rbBtnDyn:hover {        background-color: {{12}} !important;        color: {{13}} !important;        -webkit-transition: background 1s ease;        -moz-transition: background 1s ease;        -o-transition: background 1s ease;        transition: background 1s ease;     }     .rbTpGenDyn .rbInfoBtnDyn{      position: absolute;      top: 5px;      right: -130px;      display: inline-block;     }  </style>  <div class="rbBaseStyle rbTpGenDyn" >    <div class="rbBarWidth barDyn">      <p id="rbTopbarGenericNormalLeftClick" class="rbTextLeft rbDynLeftText rbDynText" >        {{14}}      </p>        <div id="rbTopbarGenericNormalRoiMiddlebutton" class ="rbClickable rbBtn rbBtnDyn"             link="{{15}}">         {{16}}       </div>          <p id="rbTopbarGenericNormalRightClick" class="rbTextRight rbDynRightText rbDynText" >                   {{17}}        </p>      <div class="rbInfoBtnDyn">        <div class="rb-btn-group" >          <button id="rbTopbarGenericNormalRoiHelp" class="rb-info-btn rb-info-btn-small rbClickable" link="http://www.rulebot.com">            ?          </button>          <button id="rbTopbarGenericNormalCloseClick" class="rb-info-btn rb-info-btn-small rbClickable" >            X          </button>                    </div>      </div>      </div>  </div></div>'
+trigger_fish.rbT.rbTemplTopbarGenericNormalHTML='<div id="rbTopbarGenericNormalBaseContainer">  <!-- -->  <link rel="stylesheet" type="text/css" href="https://s3.amazonaws.com/actwitty_ganga/css/common_styles.css">  <style>        .rbTpGenDyn{      position: fixed;      top: 0px;      left: 0px;         z-index: {{1}};          background: {{2}} !important;      border-top: 1px solid #CC412A !important;      border-bottom: 1px solid #CC412A !important;    }    .rbTpGenDyn .barDyn{      width: {{3}}px;      height: {{4}}px;      vertical-align: middle;      position: relative;    }    .rbTpGenDyn .rbDynText     {       color:{{5}};       font-size: {{6}}px;       font-family: {{7}};       font-weight:{{8}};       white-space: nowrap;       overflow:hidden;     }          .rbTpGenDyn .rbDynLeftText{      position: absolute;      left:20px;       width:40%;      top: 10px;      text-align: center;      margin-right: 20px;     }          .rbTpGenDyn .rbDynRightText{      position: absolute;      right: 20px;       top: 10px;          width:40%;       text-align: center;         margin-left: 20px;     }     .rbTpGenDyn .rbBtnDyn {          padding: 5px;          text-shadow: none !important;        border: 1px solid #BBB;        border: 1px solid #BBB;        border-color: #E6E6E6 #E6E6E6 #BFBFBF;        border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);        -webkit-border-radius: 4px;        -moz-border-radius: 4px;        border-radius: 4px;        -webkit-transition: background 1s ease;        -moz-transition: background 1s ease;        -o-transition: background 1s ease;        transition: background 1s ease;                    position: absolute;              width:100px;        left: 42%;        top:7%;        height:20px;        margin-right: 20px;                            background-color: {{9}} !important;        color: {{10}} !important; !important;        font-size: {{11}}px;         text-align: center;          white-space: nowrap;          overflow: hidden;         text-overflow: ellipsis;     }     .rbTpGenDyn .rbBtnDyn:hover {        background-color: {{12}} !important;        color: {{13}} !important;        -webkit-transition: background 1s ease;        -moz-transition: background 1s ease;        -o-transition: background 1s ease;        transition: background 1s ease;     }     .rbTpGenDyn .rbInfoBtnDyn{      position: absolute;      top: 5px;      right: -130px;      display: inline-block;     }  </style>  <div class="rbBaseStyle rbTpGenDyn" >    <div class="rbBarWidth barDyn">      <p id="rbTopbarGenericNormalLeftClick" class="rbTextLeft rbDynLeftText rbDynText" >        {{14}}      </p>        <div id="rbTopbarGenericNormalRoiMiddlebutton" class ="rbClickable rbBtn rbBtnDyn"             link="{{15}}">         {{16}}       </div>          <p id="rbTopbarGenericNormalRightClick" class="rbTextRight rbDynRightText rbDynText" >                   {{17}}        </p>      <div class="rbInfoBtnDyn">        <div class="rb-btn-group" >          <button id="rbTopbarGenericNormalRoiHelp" class="rb-info-btn rb-info-btn-small rbClickable" link="http://www.rulebot.com">            ?          </button>          <button id="rbTopbarGenericNormalCloseClick" class="rb-info-btn rb-info-btn-small rbClickable" >            X          </button>                    </div>      </div>      </div>  </div></div>'
 
 
 
@@ -4905,77 +4908,76 @@ trigger_fish.rbT.fillTheRuntimeValueForTemplArgs = function(tempMatch,actionparm
                            for(var i=0 ; i<tempMatch.length ; i++)
                            {
 
-                              var objNested = {};
-                               
-                               
-                               var tempMatchForscope = ""
+                                        var objNested = {};
+                                         
+                                         
+                                         var tempMatchForscope = ""
 
-                               tempMatch[i]=tempMatch[i].replace("{{","");
-                               tempMatch[i]=tempMatch[i].replace("}}","");
+                                         tempMatch[i]=tempMatch[i].replace("{{","");
+                                         tempMatch[i]=tempMatch[i].replace("}}","");
 
 
-                               tempMatchForscope = tempMatch[i].match(/[\w]*/g);
+                                         tempMatchForscope = tempMatch[i].match(/[\w]*/g);
 
-                               if(tempMatchForscope[0])
-                               
-                               {
-                                   var k =0;
+                                         if(tempMatchForscope[0])
+                                         
+                                         {
+                                             var k =0;
 
-                                
+                                          
 
-                                    if(tempMatchForscope[0] == "s")
-                                    {
-                                       objNested = trigger_fish.rbT.currentSystemVar; 
-   
-                                       for(k=2;k<=tempMatchForscope.length-2;k++)
-                                       {
-                                         if(k%2 === 0)
-                                          {
-                                             var objNested = objNested[tempMatchForscope[k]] 
+                                              if(tempMatchForscope[0] == "s")
+                                              {
+                                                 objNested = trigger_fish.rbT.currentSystemVar; 
+             
+                                                 for(k=2;k<=tempMatchForscope.length-2;k++)
+                                                 {
+                                                   if(k%2 === 0)
+                                                    {
+                                                       var objNested = objNested[tempMatchForscope[k]] 
 
-                                          } 
-                                       }
-                                   }
+                                                    } 
+                                                 }
+                                             }
 
-                                  else if(tempMatchForscope[0] == "e")
-                                    {
-                                       objNested =trigger_fish.rbT.currentEventVar; 
-   
-                                       for(k=2;k<=tempMatchForscope.length-2;k++)
-                                       {
-                                         if(k%2 === 0)
-                                          {
-                                             var objNested = objNested[tempMatchForscope[k]] 
+                                            else if(tempMatchForscope[0] == "e")
+                                              {
+                                                 objNested =trigger_fish.rbT.currentEventVar; 
+             
+                                                 for(k=2;k<=tempMatchForscope.length-2;k++)
+                                                 {
+                                                   if(k%2 === 0)
+                                                    {
+                                                       var objNested = objNested[tempMatchForscope[k]] 
 
-                                          } 
-                                       }
-                                   }
+                                                    } 
+                                                 }
+                                             }
 
-                                  else if(tempMatchForscope[0] == "a")
-                                    {
-                                       objNested =trigger_fish.rbT.currentActorVar ; 
-   
-                                       for(k=2;k<=tempMatchForscope.length-2;k++)
-                                       {
-                                         if(k%2 === 0)
-                                          {
-                                             var objNested = objNested[tempMatchForscope[k]] 
+                                            else if(tempMatchForscope[0] == "a")
+                                              {
+                                                 objNested =trigger_fish.rbT.currentActorVar ; 
+             
+                                                 for(k=2;k<=tempMatchForscope.length-2;k++)
+                                                 {
+                                                   if(k%2 === 0)
+                                                    {
+                                                       var objNested = objNested[tempMatchForscope[k]] 
 
-                                          } 
-                                       }
-                                   }
+                                                    } 
+                                                 }
+                                             }
 
-                                 
-                          }  
+                                           
+                                    }  
 
-                               tempMatch[i] = '{{'+ tempMatch[i] + '}}';
+                                         tempMatch[i] = '{{'+ tempMatch[i] + '}}';
 
-                               actionparmaskey = actionparmaskey.replace(tempMatch[i],objNested);
-
+                                         actionparmaskey = actionparmaskey.replace(tempMatch[i],objNested);
+                              } 
                                return actionparmaskey;
 
 
-                         }         
               
          }catch(e){
 
@@ -5617,4 +5619,6 @@ trigger_fish.rbT.invokeActionScript = function(action)
 
   
 }
+
+
 
